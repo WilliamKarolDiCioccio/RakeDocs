c7cdd996a060ceb6a4d1e7532f3ca2f0b41b56a3
Wilielmus <88447902+WilliamKarolDiCioccio@users.noreply.github.com>
2024-04-09 15:18:53 +0100

    Add partytown integration to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
-
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://docs-rake-engine.netlify.app",
   integrations: [
     starlight({
       title: "RakeDocs",
@@ -22,22 +23,43 @@ export default defineConfig({
           label: "Get Started",
           translations: {
             it: "Inizia",
-            fr: "Mise en route"
+            fr: "Mise en route",
           },
           autogenerate: {
             directory: "get-started",
           },
         },
+        {
+          label: "Architecture",
+          translations: {
+            it: "Motore Grafico",
+            fr: "Moteur Graphique",
+          },
+          autogenerate: {
+            directory: "architecture",
+          },
+        },
       ],
       defaultLocale: "root",
       locales: {
-        root: { label: "English", lang: "en" },
-        it: { label: "Italian", lang: "it" },
-        fr: { label: "French", lang: "fr" },
+        root: {
+          label: "English",
+          lang: "en",
+        },
+        it: {
+          label: "Italian",
+          lang: "it",
+        },
+        fr: {
+          label: "French",
+          lang: "fr",
+        },
       },
-      
       customCss: ["./src/tailwind.css", "./src/styles/global.css"],
     }),
-    tailwind({ applyBaseStyles: false }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    partytown(),
   ],
 });
